e32c6a3d1c1eb626d72dac553d81a01de8f3fc37
Boris Boskovic <boris.boskovic92@gmail.com>
2020-06-19 18:05:59 +0200

    initial commit

---
+++ TCP/TCP_Client CLI/TCP_Client CLI/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCP_Client_CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = new byte[1024];
+            string input, stringData;
+
+            bool connected = false;
+            while (!connected) { }
+            Console.WriteLine("Unesite adresu:");
+            string adresa = Console.ReadLine();
+            Console.WriteLine("Unesite broj porta:");
+            string broj_porta = Console.ReadLine();
+            IPEndPoint serverep = new IPEndPoint(IPAddress.Parse(adresa), Convert.ToInt32(broj_porta));
+
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(serverep);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Nije moguce spojiti se na server.");
+                Console.ReadKey();
+                return;
+            }
+
+            int recv = server.Receive(data);
+            stringData = Encoding.ASCII.GetString(data, 0, recv);
+            Console.WriteLine(stringData);
+
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "XX")
+                    break;
+                server.Send(Encoding.ASCII.GetBytes(input));
+                data = new byte[1024];
+
+                //recv = server.Receive(data);
+                //stringData = Encoding.ASCII.GetString(data, 0, recv);
+                //Console.WriteLine(stringData);
+            }
+            server.Shutdown(SocketShutdown.Both);
+            Console.WriteLine("Aplikacija se zatvara... (Pritisnite bilo koji taster)");
+            server.Close();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TCP/TCP_Server CLI/TCP_Server CLI/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCP_Server_CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = new byte[1024];
+
+            //Od korisnika se trazi da unese broj porta sve dok ne unese broj u opsegu 2500-3500
+            int port;
+            do
+            {
+                System.Console.Write("Unesite zeljeni broj porta[2500-3500]: ");
+                port = Convert.ToInt32(Console.ReadLine());
+            } while (port < 2500 || port > 3500);
+
+            //Server osluškuje na unesenom portu, za bilo koju adresu
+            IPEndPoint serverep = new IPEndPoint(IPAddress.Any, port);
+            Socket newsock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            newsock.Bind(serverep);
+            newsock.Listen(10);
+
+            do
+            {
+                Console.WriteLine("Ceka se klijent...");
+
+                //Prihvatanje klijenta i ispisivanje obavjestenja o povezivanju
+                Socket client = newsock.Accept();
+                IPEndPoint clientep = (IPEndPoint)client.RemoteEndPoint;
+                Console.WriteLine("Povezano sa {0} na portu {1}", clientep.Address, clientep.Port);
+
+                //Nakon povezivanja klijentu se salje poruka dobrodoslice
+                string welcome = "Pozdrav, uspjesno ste se povezali sa " + serverep.Address + " na portu " + serverep.Port + "\nSada mozete slati poruke";
+                data = Encoding.ASCII.GetBytes(welcome);
+                client.Send(data, data.Length, SocketFlags.None);
+
+                //Dalje se ispisuju poruke koje se prime od klijenta, sve dok ne posalje "XX"
+                while (true)
+                {
+                    data = new byte[1024];
+
+                    int recv;
+                    //Ovaj try catch sluzi da ne pukne server aplikacija ukoliko se klijent nasilno zatvori
+                    try
+                    {
+                        recv = client.Receive(data);
+                    }
+                    catch (SocketException)
+                    {
+                        break;
+                    }
+                    string tekst_data = Encoding.ASCII.GetString(data, 0, recv);
+                    if (recv == 0)
+                        break;
+
+                    Console.WriteLine(tekst_data);
+
+                    //client.Send(data, recv, SocketFlags.None);
+                }
+                Console.WriteLine("Veza sa {0} je prekinuta...", clientep.Address);
+                client.Close();
+            } while (true);
+
+        }
+    }
+}
